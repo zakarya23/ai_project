af9f04d59cf080a4e63e79fe2caf358a4ed19640
Kamy <38801575+kamyfakhr@users.noreply.github.com>
2021-03-22 00:02:38 +1100

    Update main.py
    Adding the A* algorithm function and class Node

--- search/main.py
+++ search/main.py
@@ -1,7 +1,6 @@
 """
 COMP30024 Artificial Intelligence, Semester 1, 2021
 Project Part A: Searching
-
 This script contains the entry point to the program (the code in
 `__main__.py` calls `main()`). Your solution starts here!
 """
@@ -14,14 +13,123 @@ import json
 # then import from them like this:
 from search.util import print_board, print_slide, print_swing
 
+class Node():
+    def init(curr, parent=None, position=None):
+        curr.parent = parent;
+        curr.position = position 
+        curr.type = None   #paper,rock etc.
+        curr.g = 0
+        curr.h = 0
+        curr.f = 0
+
+    #implementation of the swing movement goes here
+    def swing(curr.position):
+       
+       return None
+
+    #implementation of the slide movement goes here
+    def slide(curr.position):
+
+        return None
+
+        
+
+
+
+
+
+#This will return a list of tuples demonstrating the shortest path
+def make_solution(board, start, goal):
+    
+
+
+    #initializing start and end nodes
+    
+    start = Node(None, start)  
+    start.g = start.h = start.f = 0
+    goal = Node(None, goal)  
+    goal.g = goal.h = goal.f = 0
+
+
+    unvisited = []  #visited nodes
+    visited = []    #unvisited nodes
+
+    unvisited.append(start)
+
+    while len(unvisited) > 0:
+
+        curr = unvisited[0]
+        curr_index = 0
+
+        for index, _hex in enumerate(open_list):
+            if hex.f < curr.f:
+                curr = _hex
+                curr_index = index
+        
+        unvisited.pop(curr_index)
+        visited.append(curr)
+
+
+        if curr == goal:
+            route = []
+            curr_hex = curr
+            while curr_hex is not None:
+                route.append(curr_hex.position)
+                curr_hex = curr_hex.parent
+            return path[::-1] #this will reversed the path cause we are going towards goal
+
+
+            child_hexes = []
+
+            for next_position in [(0,1),(0,-1),(1,-1),(1,0),(-1,0),(-1,1)]
+
+                    hex_pos = (curr.position[0]+next_position[0], curr.position[1]+next_position[1])
+
+                    # we need an if node here to check if the hex is not out of the board
+
+
+                    # if board[hex_pos[0]]hex_pos[1]] != block_location : ?  to check if its not a block
+
+
+                    new_hex = Node(curr, next_position) #new hex
+
+                    child_hexes.append(new_hex)
+
+
+            #now it is time to check the child hexes 
+
+            for child in child_hexes:
+
+                for visited_child in visited:
+                    if child == visited_child:
+                        continue
+
+                
+                child.g = curr.g + 1
+                #euclidean distance between child and the goal hex  
+                child.h = ((child.position[0] - goal.position[0]) ** 2) + ((child.position[1] - goal.position[1]) ** 2)
+                child.f = child.g + child.h
+
+                #if child hex is in the unvisited list
+                for unvisited_hex in unvisited:
+                    if child == unvisited_hex and child.g > unvisited_hex.g:
+                        continue
+
+
+                unvisited.append(child)
+
+
+
+
+                
+
+
+                        
 def make_graphs(initial, goal): 
     graphs = []
     
     return graphs
 
-def make_solution(initial, goal): 
-    # Move towards the goals while keeping track of the distance
-    print(None)
 
 def main():
     try:
