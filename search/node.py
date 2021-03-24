5d183994e0f80d723b73893590e8c35198789501
Kamy <38801575+kamyfakhr@users.noreply.github.com>
2021-03-24 22:57:01 +1100

    Movements Pseudocode

--- search/node.py
+++ search/node.py
@@ -11,12 +11,20 @@ class Node():
         self.h = 0
         self.f = 0
 
-    #implementation of the swing movement goes here
-    # def swing(curr.position):
+    #implementation of the slide movement goes here, the current position of the node + the direction of the
+    # movement(left, right, south_left,south_right, north_right, north_left) 
+ 
+    # def slide(curr.position, direction):
+       # movement of one tile towards the given direction like (x+1,y) or (x, y-1), we have 6 different movements
        
     #    return None
 
-    #implementation of the slide movement goes here
-    # def slide(curr.position):
+    #implementation of the swing movement goes here 
+    #the current positon, direction of the movement
+    
+    # def swing(curr.position, direction, list of same player's tokens):
+    
+    # it should move to one of the adjacent hexes of the chosen same player's token 
+    
 
     #     return None
