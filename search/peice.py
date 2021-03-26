09160652cf70295494cfc68f16197457e9afcc54
Kamy <38801575+kamyfakhr@users.noreply.github.com>
2021-03-26 22:41:32 +1100

    Update peice.py

--- search/peice.py
+++ search/peice.py
@@ -1,10 +1,10 @@
 class Piece:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+    def __init__(self, r=0, q=0):
+        self.r = r
+        self.q = q
 
     def distance_from_origin(self):
-        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+        return ((self.r ** 2) + (self.q ** 2)) ** 0.5
 
     def print_piece(self):
-        print('({0}, {1})'.format(self.x, self.y))
+        print('({0}, {1})'.format(self.r, self.q))
