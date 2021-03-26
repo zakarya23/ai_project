aa625670efd3e32055af054143e9e0c7f1b27848
Kamy <38801575+kamyfakhr@users.noreply.github.com>
2021-03-26 21:25:02 +1100

    Implementing Piece class

--- search/peice.py
+++ search/peice.py
@@ -1,1 +1,10 @@
+class Piece:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 
+    def distance_from_origin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def print_piece(self):
+        print('({0}, {1})'.format(self.x, self.y))
