fde14fb3072ce4075dc5394d417d854271cf8057
Zakarya Ahmed Butt <zakarya.butt@student.unimelb.edu.au>
2021-04-28 13:24:48 +0500

    Trying 1

--- PartB/DOMINATORS/player.py
+++ PartB/DOMINATORS/player.py
@@ -18,14 +18,25 @@ class Player:
         # If upper is given to us we can start with x = 4 and y is anything in the given range
         if player == "upper": 
             self.start = (4, 0)
+            self.move = "SUBTRACT"
         else: 
             self.start = (-4, 0)
+            self.move = "ADD"
         
         self.player_type = player
         self.board = Board()
         self.turn = 0 
         self.first_turn = True 
+        self.throws = 0 
         
+    def estimate(self, our, opponent):
+        if our in self.board.opponents:
+            return 1
+        elif opponent in self.board.our_pieces: 
+            return - 1
+        else: 
+            return 0 
+
 
     def action(self):
         """
@@ -33,7 +44,9 @@ class Player:
         of the game, select an action to play this turn.
         """
         # if self.player_type == "lower":
+        # Need to make sure killed pieces dont exist 
         token = ["r", "s", "p"]
+        # ALSO NEED TO MAKE SURE THROWS DONT EXCEED 9 
     
 
         # put your code here
@@ -42,12 +55,21 @@ class Player:
             self.first_turn = False
             # self.turn += 1
             r_index = randrange(len(token))
-            
+            self.throws += 1
             return ("THROW", token[r_index], (self.start[0], self.start[1])) 
-        elif self.turn % 2 ==  0: 
+        elif self.turn % 2 ==  0 and self.throws < 10: 
             # How to calculate new throw position
             self.turn += 1
-            return ("THROW", "s", (self.start[0], self.start[1])) 
+            self.throws += 1
+            if (self.move == "ADD"):
+                self.start = (self.start[0] + 1, self.start[1])
+            else: 
+                 self.start = (self.start[0] - 1, self.start[1])
+
+            r_index = randrange(len(token))
+            print("AAAAAAAAAAAAAAAAAAAAA")
+            print(self.throws)
+            return ("THROW", token[r_index], (self.start[0], self.start[1])) 
         else: 
             self.turn += 1
             random_index = randrange(len(self.board.our_pieces))
@@ -58,8 +80,11 @@ class Player:
                
                 movex = t[0] + to_move.current[0] 
                 movey = t[1] + to_move.current[1] 
+                # print("AA")
                 if (movex, movey) in self.board.spots: 
                     move = (movex, movey)
+                    print(move)
+
                     return ("SLIDE", to_move.current, move)
           
         #     # Get one piece from our list 
@@ -84,8 +109,15 @@ class Player:
         """
         # put your code here
         # Means its a throw 
+        battle = False
+        p = None
+        o = None
+        if player_action[2] == opponent_action[2]: 
+            battle = True
+
         if opponent_action[0] == "THROW": 
             opp_piece = Piece(opponent_action[2], opponent_action[1]) 
+            o = opp_piece
             self.board.opponents.append(opp_piece)  
         else: 
             old_location = opponent_action[1]
@@ -95,8 +127,11 @@ class Player:
                 if piece.current == old_location:  
                     piece.current = opponent_action[2] 
 
+        # Also need to make sure killed pieces dissapear 
+
         if player_action[0] == 'THROW': 
-            p_piece =  Piece(player_action[2], player_action[1])   
+            p_piece =  Piece(player_action[2], player_action[1])  
+            p = p_piece 
             self.board.our_pieces.append(p_piece)
         else: 
             old_location = player_action[1]
@@ -105,3 +140,13 @@ class Player:
                 # If location matches we update its position
                 if piece.current == old_location:  
                     piece.current = player_action[2] 
+                    p = piece
+
+        if battle: 
+            print("battlebattlebattlebattlebattlebattlebattlebattle")
+            if p.name == "r" and o.name == "s" or p.name == "p" and o.name == "r" or p.name == "s" and o.name == "p" :
+                ind = self.board.opponents.index(o)
+                self.board.opponents.pop(ind)
+
+
+
