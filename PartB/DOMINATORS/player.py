f6bc03f3da5c9cc16e03849b11b73741e12f01e8
Zakarya Ahmed Butt <zakarya.butt@student.unimelb.edu.au>
2021-04-29 01:01:58 +0500

    Just need to fix battles

--- PartB/DOMINATORS/player.py
+++ PartB/DOMINATORS/player.py
@@ -46,8 +46,12 @@ class Player:
         # if self.player_type == "lower":
         # Need to make sure killed pieces dont exist 
         token = ["r", "s", "p"]
+        sent = False
         # ALSO NEED TO MAKE SURE THROWS DONT EXCEED 9 
-    
+
+
+        # So if in thhe same turn a piece is killed without 
+        # update being called we get erorr 
 
         # put your code here
         # How to decide whether to throw or slide 
@@ -56,25 +60,33 @@ class Player:
             # self.turn += 1
             r_index = randrange(len(token))
             self.throws += 1
+            sent = True
             return ("THROW", token[r_index], (self.start[0], self.start[1])) 
         elif self.turn % 2 ==  0 and self.throws < 10: 
             # How to calculate new throw position
             self.turn += 1
             self.throws += 1
             if (self.move == "ADD"):
-                self.start = (self.start[0] + 1, self.start[1])
+                new = (self.start[0] + 1, self.start[1])
+                if new in self.board.spots: 
+                    sent = True
+                    self.start = new
             else: 
-                 self.start = (self.start[0] - 1, self.start[1])
+                new =  self.start = (self.start[0] - 1, self.start[1])
+                if new in self.board.spots:
+                    sent = True 
+                    self.start = new
 
             r_index = randrange(len(token))
-            print("AAAAAAAAAAAAAAAAAAAAA")
-            print(self.throws)
+            # print("AAAAAAAAAAAAAAAAAAAAA")
+            # print(self.throws)
             return ("THROW", token[r_index], (self.start[0], self.start[1])) 
-        else: 
+        elif not sent: 
             self.turn += 1
             random_index = randrange(len(self.board.our_pieces))
             to_move = self.board.our_pieces[random_index]
             target = [(0,1),(0,-1),(1,-1),(1,0),(-1,0),(-1,1)]
+            print(f'select = {to_move.current}')
             
             for t in target: 
                
@@ -112,6 +124,8 @@ class Player:
         battle = False
         p = None
         o = None
+
+        # print(f'{player_action[2]}   {opponent_action[2]}')
         if player_action[2] == opponent_action[2]: 
             battle = True
 
@@ -121,13 +135,20 @@ class Player:
             self.board.opponents.append(opp_piece)  
         else: 
             old_location = opponent_action[1]
+            # print("SLIDE opp")
             # Searching for which piece had that initial location
             for piece in self.board.opponents: 
+                # print("FOUND WHICH PIECE FOR OPP")
                 # If location matches we update its position
-                if piece.current == old_location:  
+                if piece.current == old_location: 
+                    # print("CHASNGED OPP") 
                     piece.current = opponent_action[2] 
+                    # print(piece.current)
+                    o = piece
+                    break 
 
-        # Also need to make sure killed pieces dissapear 
+        # Also need to make sure killed pieces dissapear
+        # Make sure upper chooses upper  
 
         if player_action[0] == 'THROW': 
             p_piece =  Piece(player_action[2], player_action[1])  
@@ -135,18 +156,69 @@ class Player:
             self.board.our_pieces.append(p_piece)
         else: 
             old_location = player_action[1]
+            # print("SLIDE")
             # Searching for which piece had that initial location
             for piece in self.board.our_pieces: 
+                # print("FOUND WHICH PIECE")
                 # If location matches we update its position
                 if piece.current == old_location:  
-                    piece.current = player_action[2] 
+                    piece.current = player_action[2]
+                    # print("--------------------")
+                    # print(piece.current) 
                     p = piece
+        
+        print(len(self.board.our_pieces))
+        print(len(self.board.opponents))
 
+        # For any current battles 
         if battle: 
-            print("battlebattlebattlebattlebattlebattlebattlebattle")
+            print("battled")
             if p.name == "r" and o.name == "s" or p.name == "p" and o.name == "r" or p.name == "s" and o.name == "p" :
+                print("P1")
                 ind = self.board.opponents.index(o)
                 self.board.opponents.pop(ind)
+                battle = False
+            elif o.name == "r" and p.name == "s" or o.name == "p" and p.name == "r" or o.name == "s" and p.name == "p":
+                print("p2")
+                ind = self.board.our_pieces.index(p)
+                self.board.our_pieces.pop(ind)
+                battle = False
+
+        # FOR BATTLE WE CHECKED CURRENT PIECE NOT PREVIUS PIECEDS 
+        # Loop through opponent and our pieces to see if anything was killed in battle 
+        for pie in self.board.our_pieces:
+            if opponent_action[2] == p.current: 
+                # Battle and remove 
+                print("battled")
+                if pie.name == "r" and o.name == "s" or pie.name == "p" and o.name == "r" or pie.name == "s" and o.name == "p" :
+                    print("P1")
+                    ind = self.board.opponents.index(o)
+                    self.board.opponents.pop(ind)
+                    # battle = False
+                elif o.name == "r" and pie.name == "s" or o.name == "p" and pie.name == "r" or o.name == "s" and pie.name == "p":
+                    print("p2")
+                    ind = self.board.our_pieces.index(pie)
+                    self.board.our_pieces.pop(ind)
+                    # battle = False
+
+            # Our piece may kill itself 
+            if player_action[2] == pie.current: 
+                # Battle and remove 
+                print("battled")
+                if pie.name == "r" and p.name == "s" or pie.name == "p" and p.name == "r" or pie.name == "s" and p.name == "p" :
+                    print("P1")
+                    ind = self.board.opponents.index(o)
+                    self.board.opponents.pop(ind)
+                    # battle = False
+                elif p.name == "r" and pie.name == "s" or p.name == "p" and pie.name == "r" or p.name == "s" and pie.name == "p":
+                    print("p2")
+                    ind = self.board.our_pieces.index(p)
+                    self.board.our_pieces.pop(ind)
+
+     
+
+
+
 
 
 
